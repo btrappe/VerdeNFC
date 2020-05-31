0633cfdef9b524e2d84ff2de7eb6cc59f0622259
Bernd Trappe <Bernd.Trappe@thinprint.com>
2020-05-31 14:18:27 +0200

    cleanup data details

--- VerdeNFC/VerdeNFC/ViewModels/DataDetailsViewModel.cs
+++ VerdeNFC/VerdeNFC/ViewModels/DataDetailsViewModel.cs
@@ -16,28 +16,22 @@ namespace VerdeNFC.Views
 
             MessagingCenter.Subscribe<MainTabViewModel, byte []>(this, "DataChanged", async (obj, lmem) =>
             {
-                string ltext = "";
-                int lindex = 0;
-                for (int i = 0; i < 20; i++)
-                {
-                    ltext += string.Format("{0:D2}  {1:X2}{2:X2}{3:X2}{4:X2} \n", i, lmem[lindex], lmem[lindex + 1], lmem[lindex + 2], lmem[lindex + 3]);
-                    lindex += 4;
-                }
-
-                DataText = ltext;
+                DataText = ToString(lmem);
             });
 
-            byte[] mem = DataBag.GetData();
+            DataText = ToString(DataBag.GetData()); 
+        }
 
-            string text="";
+        protected static string ToString(byte [] mem)
+        {
+            string text = "";
             int index = 0;
             for (int i = 0; i < 20; i++)
             {
                 text += string.Format("{0:D2}  {1:X2}{2:X2}{3:X2}{4:X2} \n", i, mem[index], mem[index + 1], mem[index + 2], mem[index + 3]);
                 index += 4;
             }
-
-            DataText = text; 
+            return text;
         }
 
         public string DataText
