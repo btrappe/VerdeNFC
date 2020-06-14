5b48f15953b91e32e4349e0a02485e4b7a5a5600
trappe <btrappe@web.de>
2020-06-14 18:26:12 +0200

    Picker sets data

--- VerdeNFC/VerdeNFC/ViewModels/MainTabViewModel.cs
+++ VerdeNFC/VerdeNFC/ViewModels/MainTabViewModel.cs
@@ -12,6 +12,7 @@ using VerdeNFC.Util;
 using Plugin.Toast;
 using VerdeNFC.Models;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace VerdeNFC.ViewModels
 {
@@ -25,8 +26,8 @@ namespace VerdeNFC.ViewModels
         public event NFCControlListening NFCStartListening;
         public event NFCControlListening NFCStopListening;
 
-        private List<RoastProfile> _roastProfiles = new List<RoastProfile>();
-        public List<RoastProfile> RoastProfiles
+        private ObservableCollection<RoastProfile> _roastProfiles = new ObservableCollection<RoastProfile>();
+        public ObservableCollection<RoastProfile> RoastProfiles
         {
             get { return _roastProfiles; }
             set
@@ -50,11 +51,46 @@ namespace VerdeNFC.ViewModels
 
         public void RoastProfileSelChanged()
         {
-            if ((RoastProfileSel.Id < 100) || (RoastProfileSel.Id != 0))
+            if ((RoastProfileSel.Id < 100) && (RoastProfileSel.Id != 0))
             {
                 if ((lastSelectedRoastProfile?.Id > 100) || (lastSelectedRoastProfile?.Id == 0))
+                {
                     RoastProfiles.Remove(lastSelectedRoastProfile);
+                    OnPropertyChanged(nameof(RoastProfiles));
+                }
                 lastSelectedRoastProfile = RoastProfileSel;
+
+                byte[] mem = new byte[80];
+                int index = 16;
+                string line = RoastProfileSel.Data;
+                if (string.IsNullOrEmpty(line))
+                    return;
+
+                try
+                {
+                    int j = 0;
+                    while ((j < line.Length) && (index < 80))
+                    {
+                        if ((line[j] == ' ') || (line[j] == '\t'))
+                        {
+                            j++;
+                            continue;
+                        }
+
+                        mem[index++] = byte.Parse(line.Substring(j, 2), NumberStyles.HexNumber);
+                        j += 2;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Error: unknown line format: {0}", line);
+                }
+
+                nPause = 256 * mem[48] + mem[49];
+
+                DataBag.SetData(mem);
+                MessagingCenter.Send(this, "DataChanged", DataBag.GetData());
+
             }
             else
                 RoastProfileSel = lastSelectedRoastProfile;
@@ -188,27 +224,27 @@ namespace VerdeNFC.ViewModels
             // 1...99 - data source is Data member
             // 100..  - external data source (file or scanned NFC tag)
 
-            RoastProfiles.Add(new RoastProfile() { Id = 0, Name = "(choose one)", Data = "", isRoastProfile = false, isGrindProfile = false, isBrewProfile = false });
-            // RoastProfiles.Add(new RoastProfile() { Id = 101, Name = "(RGB from NFC or File)", Data = "", isRoastProfile = true, isGrindProfile = true, isBrewProfile = true });
-            // RoastProfiles.Add(new RoastProfile() { Id = 102, Name = "(Roast from NFC or File)", Data = "", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false });
-            // RoastProfiles.Add(new RoastProfile() { Id = 103, Name = "(Grind/Brew from NFC or File)", Data = "", isRoastProfile = false, isGrindProfile = true, isBrewProfile = true });
-            // RoastProfiles.Add(new RoastProfile() { Id = 104, Name = "(Grind from NFC or File)", Data = "", isRoastProfile = false, isGrindProfile = true, isBrewProfile = false });
-            // RoastProfiles.Add(new RoastProfile() { Id = 105, Name = "(Brew from NFC or File)", Data = "", isRoastProfile = false, isGrindProfile = false, isBrewProfile = true });
+            RoastProfiles.Add(new RoastProfile() { Id = 0, Name = "(choose one)", Data = "", isRoastProfile = false, isGrindProfile = false, isBrewProfile = false});
+            // RoastProfiles.Add(new RoastProfile() { Id = 101, Name = "(RGB from NFC or File)", Data = "", isRoastProfile = true, isGrindProfile = true, isBrewProfile = true});
+            // RoastProfiles.Add(new RoastProfile() { Id = 102, Name = "(Roast from NFC or File)", Data = "", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false});
+            // RoastProfiles.Add(new RoastProfile() { Id = 103, Name = "(Grind/Brew from NFC or File)", Data = "", isRoastProfile = false, isGrindProfile = true, isBrewProfile = true});
+            // RoastProfiles.Add(new RoastProfile() { Id = 104, Name = "(Grind from NFC or File)", Data = "", isRoastProfile = false, isGrindProfile = true, isBrewProfile = false});
+            // RoastProfiles.Add(new RoastProfile() { Id = 105, Name = "(Brew from NFC or File)", Data = "", isRoastProfile = false, isGrindProfile = false, isBrewProfile = true});
 
-            RoastProfiles.Add(new RoastProfile() { Id = 1, Name = "Brazil",                  Data = "AAB84B4B00 AAB8324B5A  AAB64B4B00 AAB6324B64 3C3C462D 32 02  1E  000501 D810 0005", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false });
-            RoastProfiles.Add(new RoastProfile() { Id = 2, Name = "80g Roast only",          Data = "AAAE4B5A05 AAAE415078  AAB4415A05 AAB4415050 37465A50 23 02  0F  000601 6113 0000", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false });
-            RoastProfiles.Add(new RoastProfile() { Id = 3, Name = "universal light roast",   Data = "AAB94B3205 AAB94B325A  AAB94B3205 AAB94B3232 3C463250 32 02  1E  000601 5D03 000A", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false });
-            RoastProfiles.Add(new RoastProfile() { Id = 4, Name = "universal medium roast",  Data = "AA645A3205 AA9632327D  AAB4463C05 AAC0323C78 376E3C2D 23 02  0F  000601 F501 000A", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false });
-            RoastProfiles.Add(new RoastProfile() { Id = 5, Name = "80g Slow Roast RGB (10h)",Data = "AAB84B4B00 AAB832465A  AAB64B5A00 AAB6324664 3C465A2D 32 01  2D  000501 CCFE 0258", isRoastProfile = true, isGrindProfile = true, isBrewProfile = true });
+            RoastProfiles.Add(new RoastProfile() { Id = 1, Name = "Brazil",                  Data = "AAB84B4B00 AAB8324B5A  AAB64B4B00 AAB6324B64 3C3C462D 32 02  1E  000501 D810 0005", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false});
+            RoastProfiles.Add(new RoastProfile() { Id = 2, Name = "80g Roast only",          Data = "AAAE4B5A05 AAAE415078  AAB4415A05 AAB4415050 37465A50 23 02  0F  000601 6113 0000", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false});
+            RoastProfiles.Add(new RoastProfile() { Id = 3, Name = "universal light roast",   Data = "AAB94B3205 AAB94B325A  AAB94B3205 AAB94B3232 3C463250 32 02  1E  000601 5D03 000A", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false});
+            RoastProfiles.Add(new RoastProfile() { Id = 4, Name = "universal medium roast",  Data = "AA645A3205 AA9632327D  AAB4463C05 AAC0323C78 376E3C2D 23 02  0F  000601 F501 000A", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false});
+            RoastProfiles.Add(new RoastProfile() { Id = 5, Name = "80g Slow Roast RGB (10h)",Data = "AAB84B4B00 AAB832465A  AAB64B5A00 AAB6324664 3C465A2D 32 01  2D  000501 CCFE 0258", isRoastProfile = true, isGrindProfile = true, isBrewProfile = true});
 
-            RoastProfiles.Add(new RoastProfile() { Id = 6, Name = "universal dark roast ",   Data = "AA645A3205 AA9632327D  AAB4463C05 AAC0323C96 376E3C2D 23 02  0F  000601 8312 000A", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false });
-            RoastProfiles.Add(new RoastProfile() { Id = 7, Name = "80g Roasters Grind/Brew", Data = "AAB0644B05 AAB0414678  AABA5F5A05 AABA41463C 376E5A2D 23 05  1E  000601 AB08 0000", isRoastProfile = false, isGrindProfile = true, isBrewProfile = true });
-            RoastProfiles.Add(new RoastProfile() { Id = 8, Name = "20g Roasters Grind/Brew", Data = "AAB44B4B00 AAB432464B  AAAE4B5A00 AAAE32465A 3C6E5A14 14 05  2D  000501 4A01 0000", isRoastProfile = false, isGrindProfile = true, isBrewProfile = true });
+            RoastProfiles.Add(new RoastProfile() { Id = 6, Name = "universal dark roast ",   Data = "AA645A3205 AA9632327D  AAB4463C05 AAC0323C96 376E3C2D 23 02  0F  000601 8312 000A", isRoastProfile = true, isGrindProfile = false, isBrewProfile = false});
+            RoastProfiles.Add(new RoastProfile() { Id = 7, Name = "80g Roasters Grind/Brew", Data = "AAB0644B05 AAB0414678  AABA5F5A05 AABA41463C 376E5A2D 23 05  1E  000601 AB08 0000", isRoastProfile = false, isGrindProfile = true, isBrewProfile = true});
+            RoastProfiles.Add(new RoastProfile() { Id = 8, Name = "20g Roasters Grind/Brew", Data = "AAB44B4B00 AAB432464B  AAAE4B5A00 AAAE32465A 3C6E5A14 14 05  2D  000501 4A01 0000", isRoastProfile = false, isGrindProfile = true, isBrewProfile = true});
 
-            RoastProfiles.Add(new RoastProfile() { Id = 90, Name = "(Brew only 80g)",        Data = "AA41644B05 AA4B413278  AA565F5005 AA5D414696 37375A2D 23 06  0F  000601 2BF2 0000", isRoastProfile = false, isGrindProfile = false, isBrewProfile = true });
-            RoastProfiles.Add(new RoastProfile() { Id = 91, Name = "(Air Filter Reset)",     Data = "AA96644B05 AA96413278  AAAC5F5005 AAB6414696 376E5A2D 23 0F  0F  000601 8D7C 0000", isRoastProfile = false, isGrindProfile = false, isBrewProfile = false });
-            RoastProfiles.Add(new RoastProfile() { Id = 92, Name = "(Maintenance: Descale)", Data = "AAB44B4B05 AAB44B465F  AAB24B5005 AAB24B4687 37465A2D 23 13  2D  000601 7F56 0007", isRoastProfile = false, isGrindProfile = false, isBrewProfile = true });
-            RoastProfiles.Add(new RoastProfile() { Id = 93, Name = "(Maintenance: Grinder clean)", Data = "AAB44B4B00 AAB432464E  AAB44B5A99 AAB4324666 3C465A2D 32 12  1E  000501 8D5E 0005", isRoastProfile = false, isGrindProfile = true, isBrewProfile = false });
+            RoastProfiles.Add(new RoastProfile() { Id = 90, Name = "(Brew only 80g)",        Data = "AA41644B05 AA4B413278  AA565F5005 AA5D414696 37375A2D 23 06  0F  000601 2BF2 0000", isRoastProfile = false, isGrindProfile = false, isBrewProfile = true});
+            RoastProfiles.Add(new RoastProfile() { Id = 91, Name = "(Air Filter Reset)",     Data = "AA96644B05 AA96413278  AAAC5F5005 AAB6414696 376E5A2D 23 0F  0F  000601 8D7C 0000", isRoastProfile = false, isGrindProfile = false, isBrewProfile = false});
+            RoastProfiles.Add(new RoastProfile() { Id = 92, Name = "(Maintenance: Descale)", Data = "AAB44B4B05 AAB44B465F  AAB24B5005 AAB24B4687 37465A2D 23 13  2D  000601 7F56 0007", isRoastProfile = false, isGrindProfile = false, isBrewProfile = true});
+            RoastProfiles.Add(new RoastProfile() { Id = 93, Name = "(Maintenance: Grinder clean)", Data = "AAB44B4B00 AAB432464E  AAB44B5A99 AAB4324666 3C465A2D 32 12  1E  000501 8D5E 0005", isRoastProfile = false, isGrindProfile = true, isBrewProfile = false});
             RoastProfileSel = RoastProfiles[0];
             lastSelectedRoastProfile = RoastProfiles[0];
 
