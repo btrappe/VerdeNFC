392154ca37697a260154d06488ab4e384545891b
trappe <btrappe@web.de>
2020-06-15 08:59:14 +0200

    working, Release ->20

--- VerdeNFC/VerdeNFC/ViewModels/MainTabViewModel.cs
+++ VerdeNFC/VerdeNFC/ViewModels/MainTabViewModel.cs
@@ -93,7 +93,7 @@ namespace VerdeNFC.ViewModels
                 }
 
                 nPause = 256 * mem[48] + mem[49];
-                //                SetControlsVisibility(mem[41]);
+                SetControlsVisibility(mem[41], false);
 
                 DataBag.SetData(mem);
                 MessagingCenter.Send(this, "DataChanged", DataBag.GetData());
@@ -244,7 +244,7 @@ namespace VerdeNFC.ViewModels
         public MainTabViewModel()
         {
             Title = "VerdeNFC 1.0";
-//            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+
             OpenFilePickerSrc = new Command(async () => await OpenFilePickerSrcAsync());
             OpenFilePickerDest = new Command(async () => await OpenFilePickerDestAsync());
             cbMultiUse = true;
@@ -258,26 +258,21 @@ namespace VerdeNFC.ViewModels
             // 100..  - external data source (file or scanned NFC tag)
 
             RoastProfiles.Add(new RoastProfile() { Id = 0, Name = "(choose one)", Data = "", isManualChoiceAllowed = false });
-            // RoastProfiles.Add(new RoastProfile() { Id = 101, Name = "(RGB from NFC or File)", Data = "", isManualChoiceAllowed = false});
-            // RoastProfiles.Add(new RoastProfile() { Id = 102, Name = "(Roast from NFC or File)", Data = "", isManualChoiceAllowed = false});
-            // RoastProfiles.Add(new RoastProfile() { Id = 103, Name = "(Grind/Brew from NFC or File)", Data = "", isManualChoiceAllowed = false});
-            // RoastProfiles.Add(new RoastProfile() { Id = 104, Name = "(Grind from NFC or File)", Data = "", isManualChoiceAllowed = false});
-            // RoastProfiles.Add(new RoastProfile() { Id = 105, Name = "(Brew from NFC or File)", Data = "", isManualChoiceAllowed = false});
 
             RoastProfiles.Add(new RoastProfile() { Id = 1, Name = "Brazil",                        Data = "AAB84B4B00 AAB8324B5A  AAB64B4B00 AAB6324B64 3C3C462D 32 02  1E  000501 D810 0005", isManualChoiceAllowed = true });
             RoastProfiles.Add(new RoastProfile() { Id = 2, Name = "80g Roast only",                Data = "AAAE4B5A05 AAAE415078  AAB4415A05 AAB4415050 37465A50 23 02  0F  000601 6113 0000", isManualChoiceAllowed = true });
             RoastProfiles.Add(new RoastProfile() { Id = 3, Name = "universal light roast",         Data = "AAB94B3205 AAB94B325A  AAB94B3205 AAB94B3232 3C463250 32 02  1E  000601 5D03 000A", isManualChoiceAllowed = true });
             RoastProfiles.Add(new RoastProfile() { Id = 4, Name = "universal medium roast",        Data = "AA645A3205 AA9632327D  AAB4463C05 AAC0323C78 376E3C2D 23 02  0F  000601 F501 000A", isManualChoiceAllowed = true });
-            RoastProfiles.Add(new RoastProfile() { Id = 5, Name = "80g Slow Roast RGB (10h)",      Data = "AAB84B4B00 AAB832465A  AAB64B5A00 AAB6324664 3C465A2D 32 01  2D  000501 CCFE 0258", isManualChoiceAllowed = true });
+            RoastProfiles.Add(new RoastProfile() { Id = 5, Name = "universal dark roast ",         Data = "AA645A3205 AA9632327D  AAB4463C05 AAC0323C96 376E3C2D 23 02  0F  000601 8312 000A", isManualChoiceAllowed = true });
+            RoastProfiles.Add(new RoastProfile() { Id = 6, Name = "80g Slow Roast RGB (10h)",      Data = "AAB84B4B00 AAB832465A  AAB64B5A00 AAB6324664 3C465A2D 32 01  2D  000501 CCFE 0258", isManualChoiceAllowed = true });
 
-            RoastProfiles.Add(new RoastProfile() { Id = 6, Name = "universal dark roast ",         Data = "AA645A3205 AA9632327D  AAB4463C05 AAC0323C96 376E3C2D 23 02  0F  000601 8312 000A", isManualChoiceAllowed = true });
             RoastProfiles.Add(new RoastProfile() { Id = 7, Name = "80g Roasters Grind/Brew",       Data = "AAB0644B05 AAB0414678  AABA5F5A05 AABA41463C 376E5A2D 23 05  1E  000601 AB08 0000", isManualChoiceAllowed = true });
             RoastProfiles.Add(new RoastProfile() { Id = 8, Name = "20g Roasters Grind/Brew",       Data = "AAB44B4B00 AAB432464B  AAAE4B5A00 AAAE32465A 3C6E5A14 14 05  2D  000501 4A01 0000", isManualChoiceAllowed = true });
 
-            RoastProfiles.Add(new RoastProfile() { Id = 90, Name = "(Brew only 80g)",              Data = "AA41644B05 AA4B413278  AA565F5005 AA5D414696 37375A2D 23 06  0F  000601 2BF2 0000", isManualChoiceAllowed = true });
-            RoastProfiles.Add(new RoastProfile() { Id = 91, Name = "(Air Filter Reset)",           Data = "AA96644B05 AA96413278  AAAC5F5005 AAB6414696 376E5A2D 23 0F  0F  000601 8D7C 0000", isManualChoiceAllowed = true });
-            RoastProfiles.Add(new RoastProfile() { Id = 92, Name = "(Maintenance: Descale)",       Data = "AAB44B4B05 AAB44B465F  AAB24B5005 AAB24B4687 37465A2D 23 13  2D  000601 7F56 0007", isManualChoiceAllowed = true });
-            RoastProfiles.Add(new RoastProfile() { Id = 93, Name = "(Maintenance: Grinder clean)", Data = "AAB44B4B00 AAB432464E  AAB44B5A99 AAB4324666 3C465A2D 32 12  1E  000501 8D5E 0005", isManualChoiceAllowed = true });
+            RoastProfiles.Add(new RoastProfile() { Id = 90, Name = "Brew only 80g",                Data = "AA41644B05 AA4B413278  AA565F5005 AA5D414696 37375A2D 23 06  0F  000601 2BF2 0000", isManualChoiceAllowed = true });
+            RoastProfiles.Add(new RoastProfile() { Id = 91, Name = "Maintenance: Air Filter",      Data = "AA96644B05 AA96413278  AAAC5F5005 AAB6414696 376E5A2D 23 0F  0F  000601 8D7C 0000", isManualChoiceAllowed = true });
+            RoastProfiles.Add(new RoastProfile() { Id = 92, Name = "Maintenance: Descale",         Data = "AAB44B4B05 AAB44B465F  AAB24B5005 AAB24B4687 37465A2D 23 13  2D  000601 7F56 0007", isManualChoiceAllowed = true });
+            RoastProfiles.Add(new RoastProfile() { Id = 93, Name = "Maintenance: Grinder clean",   Data = "AAB44B4B00 AAB432464E  AAB44B5A99 AAB4324666 3C465A2D 32 12  1E  000501 8D5E 0005", isManualChoiceAllowed = true });
             RoastProfileSel = RoastProfiles[0];
             lastSelectedRoastProfile = RoastProfiles[0];
 
@@ -397,7 +392,7 @@ namespace VerdeNFC.ViewModels
             return;
         }
 
-        public void SetControlsVisibility(byte Type)
+        public void SetControlsVisibility(byte Type, bool externalData=true)
         {
             RoastProfile r;
             switch (Type)
@@ -428,17 +423,17 @@ namespace VerdeNFC.ViewModels
                     cbMultiUseEnabled = true;
                     break;
                 case 0x0f:
-                    r = new RoastProfile() { Id = 91, Name = "(Air Filter Reset)", Data = "AA96644B05 AA96413278  AAAC5F5005 AAB6414696 376E5A2D 23 0F  0F  000601 8D7C 0000", isManualChoiceAllowed = true };
+                    r = new RoastProfile() { Id = 91, Name = "Air Filter Reset", Data = "AA96644B05 AA96413278  AAAC5F5005 AAB6414696 376E5A2D 23 0F  0F  000601 8D7C 0000", isManualChoiceAllowed = true };
                     nPauseEnabled = false;
                     cbMultiUseEnabled = true;
                     break;
                 case 0x12:
-                    r = new RoastProfile() { Id = 93, Name = "(Maintenance: Grinder clean)", Data = "AAB44B4B00 AAB432464E  AAB44B5A99 AAB4324666 3C465A2D 32 12  1E  000501 8D5E 0005", isManualChoiceAllowed = true };
+                    r = new RoastProfile() { Id = 93, Name = "Maintenance: Grinder clean", Data = "AAB44B4B00 AAB432464E  AAB44B5A99 AAB4324666 3C465A2D 32 12  1E  000501 8D5E 0005", isManualChoiceAllowed = true };
                     nPauseEnabled = false;
                     cbMultiUseEnabled = true;
                     break;
                 case 0x13:
-                    r = new RoastProfile() { Id = 92, Name = "(Maintenance: Descale)", Data = "AAB44B4B05 AAB44B465F  AAB24B5005 AAB24B4687 37465A2D 23 13  2D  000601 7F56 0007", isManualChoiceAllowed = true };
+                    r = new RoastProfile() { Id = 92, Name = "Maintenance: Descale", Data = "AAB44B4B05 AAB44B465F  AAB24B5005 AAB24B4687 37465A2D 23 13  2D  000601 7F56 0007", isManualChoiceAllowed = true };
                     nPauseEnabled = false;
                     cbMultiUseEnabled = true;
                     break;
@@ -461,10 +456,13 @@ namespace VerdeNFC.ViewModels
                     break;
             }
             
-            if (RoastProfiles.Where(p => p.Id == r.Id).FirstOrDefault() == null)
-                RoastProfiles.Add(r);
+            if (externalData)
+            {
+                if (RoastProfiles.Where(p => p.Id == r.Id).FirstOrDefault() == null)
+                    RoastProfiles.Add(r);
 
-            RoastProfileSel = RoastProfiles.Where(p => p.Id == r.Id).First();
+                RoastProfileSel = RoastProfiles.Where(p => p.Id == r.Id).First();
+            }
         }
         public static byte [] MergeTagData(byte [] mem1stCard, byte [] mem)
         {
